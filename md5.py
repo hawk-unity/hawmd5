fc7d38a420e1619fd644001ebac35d94d2e8c1b3
hawk.0fcx <67156465+hawk-0fcx@users.noreply.github.com>
2021-01-08 01:34:58 +0300

    Add files via upload

---
+++ md5.py
@@ -0,0 +1,48 @@
+import hashlib
+import os
+os.system("clear")
+print("""
++++++++++++++++++++++++++++++++
++                             +
++         HAWK OFCX           +
++              MD5            +
++                             +
++++++++++++++++++++++++++++++++
++++++++++++++++++++++++++++++++
++                             +
++        HAWK OFCX            +
++          1. MD5 OLUŞTUR     +
++                             +
++++++++++++++++++++++++++++++++
++++++++++++++++++++++++++++++++
++                             +
++        HAWK OFCX            +
++          2. MD5 KIR         +
++                             +
++++++++++++++++++++++++++++++++
+""")
+veri = input("Islem Numarasini Girin : ")
+if (veri =="1"):
+        
+    user = input("YAZIYI GİR :  ")
+    h = hashlib.md5(user.encode())
+    h2 = h.hexdigest()        
+    print(h2)
+                     
+if (veri =="2"):
+                            
+    wlist = input("wordlist: ")
+    hash2crack = input("hash: ")
+    wlistlines = open(wlist, "r").readlines()
+                
+                                              
+    for i in range(0, len(wlistlines)):
+                                          
+        hash2comp = hashlib.md5(wlistlines[i].replace(           
+                "\n", "").encode()).hexdigest()        
+                               
+        if hash2crack == hash2comp:                                                       
+            print("password found: "+wlistlines[i].replace("\n", ""))                               
+            exit()                              
+            print("password not found")
+        
