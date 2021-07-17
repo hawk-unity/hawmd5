df155b4d2eb4927667cdd1e97f1e8161df303df9
hawk.0fcx <67156465+hawk-unity@users.noreply.github.com>
2021-07-18 02:23:25 +0300

    Update md5.py

--- md5.py
+++ md5.py
@@ -1,35 +1,38 @@
 import hashlib
 import os
+import json
+import subprocess
+import urllib3
+import requests
 os.system("clear")
 print("""
-+++++++++++++++++++++++++++++++
-+                             +
+_______________________________
+|        hacktivizm.org       |
+_______________________________
 +         HAWK OFCX           +
-+              MD5            +
-+                             +
++         MD5 - CRACKER       +
 +++++++++++++++++++++++++++++++
-+++++++++++++++++++++++++++++++
-+                             +
 +        HAWK OFCX            +
 +          1. MD5 OLUŞTUR     +
-+                             +
 +++++++++++++++++++++++++++++++
-+++++++++++++++++++++++++++++++
-+                             +
 +        HAWK OFCX            +
 +          2. MD5 KIR         +
-+                             +
++          (BRUTE FORCE)      +
 +++++++++++++++++++++++++++++++
++        HAWK OFCX            +
++          3. MD5 KIR (APİ)   +
++++++++++++++++++++++++++++++++
+
 """)
 veri = input("Islem Numarasini Girin : ")
-if (veri =="1"):
+if veri =="1" :
         
     user = input("YAZIYI GİR :  ")
     h = hashlib.md5(user.encode())
     h2 = h.hexdigest()        
     print(h2)
                      
-if (veri =="2"):
+if veri =="2":
                             
     wlist = input("wordlist: ")
     hash2crack = input("hash: ")
@@ -42,7 +45,22 @@ if (veri =="2"):
                 "\n", "").encode()).hexdigest()        
                                
         if hash2crack == hash2comp:                                                       
-            print("password found: "+wlistlines[i].replace("\n", ""))                               
+            print("Cracklendi: "+wlistlines[i].replace("\n", ""))                               
+            exit()                              
+            print("Cracklenemedi....")
+if veri == "3" : 
+    dat2 = input("Kırılacak MD5 Hash ==> ")
+    print("")
+    print("_______________________________")
+    print("")
+    response = requests.get('http://www.nitrxgen.net/md5db/' + dat2).text
+    print("crack sonucu => " , response)
+    print("")
+    print("cracklenen hash => " ,dat2)
+    print("")
+    print("_______________________________")
+
+
             exit()                              
             print("password not found")
         
